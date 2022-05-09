7deff21efd4ee35993abcbafa189f46721506502
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-09 17:38:37 +0300

    fixing build

---
+++ config/project.py
@@ -0,0 +1,4 @@
+project_name = "java-cds"
+project_long_description = "checking out the cds features of Java"
+project_year_started = 2021
+project_description = "checking out the cds features of Java"
