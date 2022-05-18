d57d928f2e7c37d19d792432c772093c7542b6fd
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:00 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "java-cds"
-project_long_description = "checking out the cds features of Java"
-project_year_started = 2021
-project_description = "checking out the cds features of Java"
+description_short = "checking out the cds features of Java"
